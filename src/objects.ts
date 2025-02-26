8da2fe72fe4e7235ea674962d24c93017edf880c
Abrar <167377939+AbrarIsTired@users.noreply.github.com>
2025-02-26 17:02:09 -0500

    Update objects.ts again

--- src/objects.ts
+++ src/objects.ts
@@ -42,16 +42,10 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (question.type === "short_answer_question") {
         return true;
     }
-
-    if (question.type === "multiple_choice_question") {
-        return question.options.includes(answer);
-    }
-
-    return false;
+    return question.options.includes(answer);
 }
 
 /**
